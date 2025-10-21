08cf74ecb9fa6a7a52a4395c22d7171c96e259e1
Lin <nannaphat.wong@bumail.net>
2025-10-21 19:36:01 +0700

    God ant can walk

--- Assets/Game platform/Scripts/Ant.cs
+++ Assets/Game platform/Scripts/Ant.cs
@@ -7,11 +7,19 @@ public class Ant : Enemy
     private void Start()
     {
         base.Initialize(20);
+
+        DamageHit = (20);
+        velocity = new Vector2(-1.0f, 0.0f);
     }
 
     public override void Behavior()
     {
-        throw new System.NotImplementedException();
+        rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+    }
+
+    private void FixedUpdate()
+    {
+        Behavior();
     }
     private void Update()
     {
