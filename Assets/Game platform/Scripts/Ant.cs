6d355b360e7f887051a5d1d5f1b877a1a66f6cae
Lin <nannaphat.wong@bumail.net>
2025-10-21 19:52:07 +0700

    ant go pA pB

--- Assets/Game platform/Scripts/Ant.cs
+++ Assets/Game platform/Scripts/Ant.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class Ant : Enemy
@@ -15,14 +16,30 @@ public class Ant : Enemy
     public override void Behavior()
     {
         rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
+        if (velocity.x < 0 && rb.position.x <= MovePoint[0].position.x)
+        {
+            Flip();
+        }
+        if (velocity.x > 0 && rb.position.x >= MovePoint[1].position.x)
+        {
+            Flip();
+        }
     }
+    void Flip()
+    {
+        velocity.x *= -1;
 
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
     private void FixedUpdate()
     {
         Behavior();
+        
     }
     private void Update()
     {
-        
+       // Debug.Log(Time.deltaTime);
     }
 }
