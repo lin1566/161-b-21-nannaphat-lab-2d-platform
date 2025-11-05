7bc2eb5368605fcee6e9ebc5ba7d8509d1619ccc
Lin <nannaphat.wong@bumail.net>
2025-11-05 12:38:52 +0700

    rock 1

--- Assets/Game platform/Scripts/Banana.cs
+++ Assets/Game platform/Scripts/Banana.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 public class Banana : Weapon
 {
     // 1 reference
+    [SerializeField] private float speed;
     public override void Move()
     {
-        throw new System.NotImplementedException();
+        float newX = transform.position.x + speed * Time.fixedDeltaTime;
+        float newY = transform.position.y;
+        Vector2 newPosition = new Vector2(newX, newY);
+        transform.position = newPosition;
     }
 
     // 2 references
     public override void OnHitWith(Character character)
     {
-        throw new System.NotImplementedException();
+        if (character is Enemy)
+            character.TakeDamage(this.damage);
+    }
+    private void Start()
+    {
+        speed = 4.0f * GetShootDirection();
+        damage = 30;
+    }
+    private void FixedUpdate()
+    {
+         Move();
     }
 }
--- Assets/Game platform/Scripts/Croccodile.cs
+++ Assets/Game platform/Scripts/Croccodile.cs
@@ -1,20 +1,32 @@
 using UnityEngine;
 
-public class Croccodile:Enemy
+public class Croccodile:Enemy, IShootable
 {
     [SerializeField] private float atkRange;
     public Player player;
+
+    [field: SerializeField] public GameObject Bullet { get; set; }
+    [field: SerializeField] public Transform ShootPoint { get; set; }
+    public float ReloadTime { get; set; }
+    public float WaitTime { get; set; }
+
     private void Start()
     {
         base.Initialize(50);
         DamageHit = 30;
+
         //set atk range and target
         atkRange = 6.0f;
         player = GameObject.FindFirstObjectByType<Player>();
+
+        WaitTime = 0.0f;
+        ReloadTime = 5.0f;
+       
     }
 
     private void FixedUpdate()
     {
+       WaitTime = Time.fixedDeltaTime;  
         Behavior();
     }
 
@@ -29,10 +41,14 @@ public class Croccodile:Enemy
     }
     public void Shoot()
     {
-        Debug.Log($"{this.name} shoots rock to the {player.name}!");
+        if (WaitTime >= ReloadTime)
+        {
+            anin.SetTrigger("Shoot"); //call Shoot animation
+            var bullet = Instantiate(Bullet, ShootPoint.position, Quaternion.identity);
+            Rock rock = bullet.GetComponent<Rock>();
+            rock.InitWeapon(30, this);
+            WaitTime = 0;
+        }
     }
-    private void Update()
-    {
-        Behavior();
-    }
+   
 }
